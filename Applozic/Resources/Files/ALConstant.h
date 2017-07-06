03d37c49760700ad4a11905aec7da4465629c30c
adarshmishra <adarsh@applozic.com>
2017-07-06 17:03:21 +0530

    #27,AL-1709 Applozic Release

--- Applozic/Resources/Files/ALConstant.h
+++ Applozic/Resources/Files/ALConstant.h
@@ -7,7 +7,7 @@
 
 #import "ALUserDefaultsHandler.h"
 
-#define APPLOZIC_SDK_VERSION @"3.8.6"
+#define APPLOZIC_SDK_VERSION @"3.9"
 
 #define KBASE_URL ([ALUserDefaultsHandler getBASEURL])
 #define MQTT_URL ([ALUserDefaultsHandler getMQTTURL])
