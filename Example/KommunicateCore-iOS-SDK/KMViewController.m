3c47e9459c378eea8f7bc65d5c1c5f9b25db9399
Kirti Shilwant <shilwantkirti@gmail.com>
2021-11-23 14:06:39 +0530

    Add code to login, fetch message list and logout in the demo app.

--- Example/KommunicateCore-iOS-SDK/KMViewController.m
+++ Example/KommunicateCore-iOS-SDK/KMViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "KMViewController.h"
+#import "ApplozicClient.h"
+#import "KMAppDelegate.h"
 
 @interface KMViewController ()
+
+@property(strong, nonatomic) ApplozicClient *client;
 
 @end
 
@@ -17,13 +21,39 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [_client initWithApplicationKey:@"2faa0ef06918df6dd5dc8506df6cec267"];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)loginButtonTapped:(id)sender {
+    
+    ALUser *user = [[ALUser alloc] initWithUserId:@"test" password:@"1234" email:@"test@test.com" andDisplayName:@"sample user"];
+    [_client loginUser: user withCompletion:^(ALRegistrationResponse *rResponse, NSError *error) {
+        NSLog(@"%@", rResponse);
+    }];
+}
+
+-(IBAction)fetchMessageList:(id)sender {
+        
+    if ([ALUserDefaultsHandler isLoggedIn]) {
+        NSLog(@"User already logged in");
+        NSLog(@"Fetching message list...");
+        ALMessageService *messageService = [[ALMessageService alloc] init];
+        [messageService getLatestMessageForUser:@"test"];
+    }
+}
+
+-(IBAction)logOut:(id)sender {
+
+    [_client logoutUserWithCompletion:^(NSError *error, ALAPIResponse *response) {
+        NSLog(error);
+    }];
+    
 }
 
 @end
