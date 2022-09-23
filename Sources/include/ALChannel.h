963b8ab9a7925044c3e80b605de2e8bc0a2592fd
Sathyan Elangovan <sathya576@gmail.com>
2022-09-23 16:58:48 +0530

    Fixed un opened conversation being listed on agent app

--- Sources/include/ALChannel.h
+++ Sources/include/ALChannel.h
@@ -16,6 +16,8 @@ static NSString *const AL_CHANNEL_CONVERSATION_STATUS = @"CONVERSATION_STATUS";
 static NSString *const AL_CATEGORY = @"AL_CATEGORY";
 static NSString *const AL_CONTEXT_BASED_CHAT = @"AL_CONTEXT_BASED_CHAT";
 static NSString *const AL_CONVERSATION_ASSIGNEE = @"CONVERSATION_ASSIGNEE";
+//Channel which doesn't have user messages, only contains bot wecome messages
+static NSString *const UN_OPENED_CONVERSATION_STATUS = @"1";
 
 /*********************
  type = 7 SPECIAL CASE
